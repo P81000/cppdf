05baec671d3d4ebd95a4ffa175fb96dc84dbae37
Pedro Masteguin <ph.masteguin@gmail.com>
2026-07-30 19:29:47 -0300

    chore: cleaning project

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
-#include <string>
-#include <vector>
-
-#include "logger.hpp"
+#include "print"
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        log_error("PDF file not provided");
-        log_warning("Usage: cppdf <path-to-pdf-file>.pdf");
-        return 1;
-    }
-
-    std::string pdf_path = argv[1];
-    log_debug("Trying to open %s", pdf_path.c_str());
+    std::print("{}\n", "Hello, World");
     return 0;
 }
