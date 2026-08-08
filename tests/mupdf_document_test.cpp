6b92b95c2a5a26138404a616024107e072f33fad
Pedro Masteguin <ph.masteguin@gmail.com>
2026-08-08 18:47:21 -0300

    test(document): add unit tests for mupdf wrapper

---
+++ tests/mupdf_document_test.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include "document/mupdf_document.hpp"
+
+using namespace cppdf;
+
+TEST(MuPdfDocumentTest, HandlesInvalidFileGracefully) {
+    MuPdfDocument doc;
+
+    auto result = doc.open("/path/to/absolute/nowhere/ghost.pdf");
+
+    EXPECT_FALSE(result.has_value());
+
+    EXPECT_EQ(result.error(), DocumentError::FileNotFound);
+}
+
+TEST(MuPdfDocumentTest, PageCountIsZeroWhenEmpty) {
+    MuPdfDocument doc;
+
+    EXPECT_EQ(doc.get_page_count(), 0);
+}
